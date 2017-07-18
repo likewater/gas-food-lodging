d61ebf9c126bef10c29376103a86775a105f87d7
likewater <lakebayomackway@gmail.com>
2017-07-17 21:43:30 -0700

    add permission check for location requests

--- app/src/main/java/com/likewater/gasfoodlodging/MainMapActivity.java
+++ app/src/main/java/com/likewater/gasfoodlodging/MainMapActivity.java
@@ -11,8 +11,11 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -35,7 +38,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainMapActivity extends Activity implements LocationListener{
+public class MainMapActivity extends Activity implements LocationListener {
 
     private int userIcon, foodIcon, gasIcon, lodgingIcon, otherIcon;
     private GoogleMap theMap;
@@ -56,9 +59,9 @@ public class MainMapActivity extends Activity implements LocationListener{
         lodgingIcon = R.drawable.lodging_icon;
         otherIcon = R.drawable.purple_point;
 
-        if(theMap==null){
-            theMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.the_map)).getMap();
-            if(theMap!=null){
+        if (theMap == null) {
+            theMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.the_map)).getMap();
+            if (theMap != null) {
                 //ok - proceed
                 theMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                 placeMarkers = new Marker[MAX_PLACES];
@@ -72,27 +75,30 @@ public class MainMapActivity extends Activity implements LocationListener{
         Log.v("MainMapActivity", "location changed");
         updatePlaces();
     }
+
     @Override
-    public void onProviderDisabled(String provider){
+    public void onProviderDisabled(String provider) {
         Log.v("MainMapActivity", "provider disabled");
     }
+
     @Override
     public void onProviderEnabled(String provider) {
         Log.v("MainMapActivity", "provider enabled");
     }
+
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         Log.v("MainMapActivity", "status changed");
     }
 
-    private void updatePlaces(){
+    private void updatePlaces() {
         //update location
-        locMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locMan = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Location lastLoc = locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         double lat = lastLoc.getLatitude();
         double lng = lastLoc.getLongitude();
         LatLng lastLatLng = new LatLng(lat, lng);
-        if(userMarker!=null) userMarker.remove();
+        if (userMarker != null) userMarker.remove();
         userMarker = theMap.addMarker(new MarkerOptions()
                 .position(lastLatLng)
                 .title("You are here")
@@ -100,21 +106,31 @@ public class MainMapActivity extends Activity implements LocationListener{
                 .snippet("Your last recorded location"));
         theMap.animateCamera(CameraUpdateFactory.newLatLng(lastLatLng), 3000, null);
 
-        String placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/"+
-                "json?location="+lat+","+lng+
-                "&radius=1000&sensor=true"+
-                "&types=gas_station%7Clodging%7Cfood"+
+        String placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/" +
+                "json?location=" + lat + "," + lng +
+                "&radius=1000&sensor=true" +
+                "&types=gas_station%7Clodging%7Cfood" +
                 "&key=AIzaSyA9qlyBGNGDhzNqPleM53d5tU3z72K4zVA";
 
         new GetPlaces().execute(placesSearchStr);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 100, this);
     }
 
     private class GetPlaces extends AsyncTask<String, Void, String> {
         @Override
-        protected String doInBackground(String... placesURL){
+        protected String doInBackground(String... placesURL) {
             StringBuilder placesBuilder = new StringBuilder();
-            for (String placeSearchURL : placesURL){
+            for (String placeSearchURL : placesURL) {
                 HttpClient placesClient = new DefaultHttpClient();
                 try {
                     HttpGet placesGet = new HttpGet(placeSearchURL);
@@ -126,21 +142,21 @@ public class MainMapActivity extends Activity implements LocationListener{
                         InputStreamReader placesInput = new InputStreamReader(placesContent);
                         BufferedReader placesReader = new BufferedReader(placesInput);
                         String lineIn;
-                        while ((lineIn = placesReader.readLine()) !=null) {
+                        while ((lineIn = placesReader.readLine()) != null) {
                             placesBuilder.append(lineIn);
                         }
                     }
-                }
-                catch(Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
             return placesBuilder.toString();
         }
+
         protected void onPostExecute(String result) {
-            if(placeMarkers!=null){
-                for(int pm=0; pm<placeMarkers.length; pm++){
-                    if(placeMarkers[pm]!=null)
+            if (placeMarkers != null) {
+                for (int pm = 0; pm < placeMarkers.length; pm++) {
+                    if (placeMarkers[pm] != null)
                         placeMarkers[pm].remove();
                 }
             }
@@ -154,17 +170,17 @@ public class MainMapActivity extends Activity implements LocationListener{
                 //marker options for each place returned
                 places = new MarkerOptions[placesArray.length()];
                 //loop through places
-                for (int p=0; p<placesArray.length(); p++) {
+                for (int p = 0; p < placesArray.length(); p++) {
                     //parse each place
                     //if any values are missing we won't show the marker
-                    boolean missingValue=false;
-                    LatLng placeLL=null;
-                    String placeName="";
-                    String vicinity="";
+                    boolean missingValue = false;
+                    LatLng placeLL = null;
+                    String placeName = "";
+                    String vicinity = "";
                     int currIcon = otherIcon;
-                    try{
+                    try {
                         //attempt to retrieve place data values
-                        missingValue=false;
+                        missingValue = false;
                         //get place at this index
                         JSONObject placeObject = placesArray.getJSONObject(p);
                         //get location section
@@ -176,19 +192,17 @@ public class MainMapActivity extends Activity implements LocationListener{
                         //get types
                         JSONArray types = placeObject.getJSONArray("types");
                         //loop through types
-                        for(int t=0; t<types.length(); t++){
+                        for (int t = 0; t < types.length(); t++) {
                             //what type is it
-                            String thisType=types.get(t).toString();
+                            String thisType = types.get(t).toString();
                             //check for particular types - set icons
-                            if(thisType.contains("gas")){
+                            if (thisType.contains("gas")) {
                                 currIcon = gasIcon;
                                 break;
-                            }
-                            else if(thisType.contains("lodging")){
+                            } else if (thisType.contains("lodging")) {
                                 currIcon = lodgingIcon;
                                 break;
-                            }
-                            else if(thisType.contains("food")){
+                            } else if (thisType.contains("food")) {
                                 currIcon = foodIcon;
                                 break;
                             }
@@ -197,36 +211,33 @@ public class MainMapActivity extends Activity implements LocationListener{
                         vicinity = placeObject.getString("vicinity");
                         //name
                         placeName = placeObject.getString("name");
-                    }
-                    catch(JSONException jse){
+                    } catch (JSONException jse) {
                         Log.v("PLACES", "missing value");
-                        missingValue=true;
+                        missingValue = true;
                         jse.printStackTrace();
                     }
                     //if values missing we don't display
-                    if(missingValue)	places[p]=null;
+                    if (missingValue) places[p] = null;
                     else
-                        places[p]=new MarkerOptions()
+                        places[p] = new MarkerOptions()
                                 .position(placeLL)
                                 .title(placeName)
                                 .icon(BitmapDescriptorFactory.fromResource(currIcon))
                                 .snippet(vicinity);
                 }
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            if(places!=null && placeMarkers!=null){
-                for(int p=0; p<places.length && p<placeMarkers.length; p++){
+            if (places != null && placeMarkers != null) {
+                for (int p = 0; p < places.length && p < placeMarkers.length; p++) {
                     //will be null if a value was missing
-                    if(places[p]!=null)
-                        placeMarkers[p]=theMap.addMarker(places[p]);
+                    if (places[p] != null)
+                        placeMarkers[p] = theMap.addMarker(places[p]);
                 }
             }
 
         }
     }
-
 
 
     @Override
@@ -247,7 +258,17 @@ public class MainMapActivity extends Activity implements LocationListener{
     @Override
     protected void onResume() {
         super.onResume();
-        if(theMap!=null){
+        if (theMap != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 100, this);
         }
     }
